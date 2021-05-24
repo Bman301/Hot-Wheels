9853eaee74ead204f8d79ce274eb9bd867305cee
ramirez <prlara2000@gmail.com>
2021-05-24 17:56:58 -0500

    add the bookings actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @car = Car.find(params[:car_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @car = Car.find(params[:car_id])
+    @booking.car = @car
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
+
+ private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date, :pickup_location, :insurance, :comment, :car_id, :user_id)
+  end
